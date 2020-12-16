a2538d2a08c9258c1b7c142de0f92eb6bf5708ab
Christopher Biessener <cpbiessener@gmail.com>
2020-12-16 11:49:13 -0600

    Bootstrap de-revved to latest stable

--- react_memory/src/components/GameHeader.js
+++ react_memory/src/components/GameHeader.js
@@ -4,14 +4,14 @@ import { Container, Row, Col } from '../components/Grid'
 const GameHeader = props => (
   <header className='header clearfix'>
   <Container>
-    <Row>
-      <Col size='4'>
+    <Row classes='justify-content-md-center'>
+      <Col size='md-auto'>
         <h2 className='left'>Click Memory</h2>
       </Col>
-      <Col size='4'>
+      <Col size='md-auto'>
         <h2 className='center'>{props.status}</h2>
       </Col>
-      <Col size='4'>
+      <Col size='md-auto'>
         <h2 className='right'>Score: {props.score.toString()} | Best: {props.topScore.toString()}</h2>
       </Col>
     </Row>
